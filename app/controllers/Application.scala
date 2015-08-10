0fbee6e1fbc03ac21c97b2ddd210abacdb35fd53
Aliaksei Bahdanau <lex.nox@gmail.com>
2015-08-11 00:48:34 +0300

    improvments for new template

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,21 @@
 package controllers
 
+import play.api.Play
 import play.api.mvc._
 import settings.DefaultSettings
+import play.api.Play.current
+
+import scala.io.Source
 
 
 class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index(DefaultSettings.defaultExtractDirectory))
+    Ok(views.html.index(DefaultSettings.defaultExtractDirectory, getDigestFormat))
+  }
+
+  private def getDigestFormat: String = {
+    val digestJsonUrl = Play.classloader.getResource("digest.json")
+    Source.fromURL(digestJsonUrl).getLines().mkString
   }
 }
